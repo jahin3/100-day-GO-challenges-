f0aa9f0ce170a315481fb8c3025bee0cee89eecc
jahin3 <jahinmoyna@gmail.com>
2026-04-28 01:56:03 +0600

    Add files via upload

---
+++ scope.go
@@ -0,0 +1,30 @@
+package main		
+import ("fmt")		
+
+var (
+	
+	a = 20	
+	b = 30
+
+)
+
+func add(x int , y int ) {
+
+	z:= x + y
+	fmt.Println(z)
+}
+
+
+func main() {
+
+  p := 30
+  q := 40
+
+  add(p, q)
+    
+  add(a, b)
+  
+  add(a , q)	
+}	
+
+	
